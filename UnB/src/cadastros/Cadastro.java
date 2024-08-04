d8c10abf2524f9a0f34058ac29fb3f806850eda9
Matheus-0217 <m.c.n0217@gmail.com>
2024-08-04 18:59:00 -0300

    segundo commit

--- UnB/src/cadastros/Cadastro.java
+++ UnB/src/cadastros/Cadastro.java
@@ -1,5 +1,29 @@
 package cadastros;
 
-public class Cadastro {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cadastro<T> {
+	private List<T> lista;
+
+    public Cadastro() {
+        this.lista = new ArrayList<>();
+    }
+
+    public void adicionar(T item) {
+        lista.add(item);
+    }
+
+    public List<T> listar() {
+        return lista;
+    }
+
+    public T buscar(int index) {
+        if (index >= 0 && index < lista.size()) {
+            return lista.get(index);
+        } else {
+            throw new IndexOutOfBoundsException("Índice fora dos limites");
+        }
+    }
 
 }
